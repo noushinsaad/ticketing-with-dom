83bdfb09b5be0ab4be7d2e21b39f2df431fc45d0
Md. Noushin Islam <noushinshoha@gmail.com>
2024-09-23 01:52:26 +0600

    made some changes in the js

--- app.js
+++ app.js
@@ -17,39 +17,39 @@ function handleSelectSeat(event) {
         return;
     }
 
+    else if (selectedSeat.length < 4) {
+        selectedSeat.push(event.innerText);
 
+        totalBookedEl.innerText = selectedSeat.length;
 
-    selectedSeat.push(event.innerText);
+        // decreasing the available seat
+        // const availableSeatValue = parseFloat(availableSeatEl.innerText);
+        // availableSeatEl.innerText=availableSeatValue-1;
+        event.classList.add('bg-primary');
+        event.classList.add('text-white');
 
-    totalBookedEl.innerText = selectedSeat.length;
+        selectedSeatEl.innerHTML += `
+        <li class="text-base font-normal flex justify-between">
+        <span>${event.innerText}</span>
+        <span>Economy</span>
+        <span>550</span>
+        </li>
+        `
+        totalPrice += 550;
+        console.log(totalPrice)
+        totalPriceEl.innerText = totalPrice.toFixed(2);
+        // console.log(selectedSeat);
 
-    // decreasing the available seat
-    // const availableSeatValue = parseFloat(availableSeatEl.innerText);
-    // availableSeatEl.innerText=availableSeatValue-1;
-    if (selectedSeat.length > 4) {
-        alert('Maximum Seat allocated');
-        return;
+        // active coupon button
+        if (selectedSeat.length > 3) {
+            couponInputEl.removeAttribute('disabled');
+            couponBtnEl.removeAttribute('disabled');
+        }
     }
 
-    event.classList.add('bg-primary');
-    event.classList.add('text-white');
-
-    selectedSeatEl.innerHTML += `
-    <li class="text-base font-normal flex justify-between">
-    <span>${event.innerText}</span>
-    <span>Economy</span>
-    <span>550</span>
-    </li>
-    `
-    totalPrice += 550;
-    console.log(totalPrice)
-    totalPriceEl.innerText = totalPrice.toFixed(2);
-    // console.log(selectedSeat);
-
-    // active coupon button
-    if (selectedSeat.length > 3) {
-        couponInputEl.removeAttribute('disabled');
-        couponBtnEl.removeAttribute('disabled');
+    else {
+        alert('Maximum Seat allocated');
+        return;
     }
 
 
